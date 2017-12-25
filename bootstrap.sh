20be49838cf49e8ec8402da69cfe2e237e9fb04f
Tommy Kronstal <tommy.kronstal@gmail.com>
2017-12-25 16:40:53 +0100

    downloads fac 1.1.4

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 
-sudo yum update -y
-sudo yum install redhat-lsb-core kernel-headers kernel-devel python-devel -y
-sudo yum groupinstall "Development Tools" -y
-sudo yum update -y
+cleanSpace() {
+    rm -rf /vagrant/fac* /vagrant/*.zip*
+}
 
-cd /vagrant/fac
+cleanSpace
+
+sudo yum update -y
+sudo yum install redhat-lsb-core kernel-headers wget kernel-devel python-devel -y
+sudo yum groupinstall "Development Tools" -y
+
+cd /vagrant
+wget https://github.com/fnevgeny/fac/archive/v1.1.4.zip
+unzip v1.1.4.zip && cd /vagrant/fac-1.1.4
+
 ./configure
 make
 sudo make install
 make pfac
 sudo make install-pfac
+
+cleanSpace
